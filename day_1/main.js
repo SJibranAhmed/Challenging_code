7c67421d68ba5f212b83c1840ffdd2a134b4b99b
SJibran Ahmed <Sheikhjibran889970@gmail.com>
2024-03-06 21:53:47 +0500

    personal message

---
+++ day_1/main.js
@@ -0,0 +1,5 @@
+// Sheihk Jibran Ahmed
+// storing name in variable
+var tutorName = 'Governer Sindh I.T intaitive';
+// print value of variable
+console.log('In ' + tutorName + ' all teachers are so work hard and sincere with all students');
---
+++ day_1/main.ts
@@ -0,0 +1,7 @@
+// Sheihk Jibran Ahmed
+
+// storing name in variable
+let tutorName:string = 'Governer Sindh I.T intaitive'
+
+// print value of variable
+console.log( 'In '+tutorName + ' all teachers are so work hard and sincere with all students.')
