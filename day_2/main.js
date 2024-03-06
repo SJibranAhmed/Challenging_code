e6127968a93ba8e5229c1a49e42349ba497294b2
SJibran Ahmed <Sheikhjibran889970@gmail.com>
2024-03-06 22:16:24 +0500

    Print value upper lower & titlecase

---
+++ day_2/main.js
@@ -0,0 +1,9 @@
+// Sheihk Jibran Ahmed
+//create variable 
+var candidateName = 'jibran';
+// print variable in lowercase
+console.log(candidateName.toLowerCase());
+// print variable in uppercase
+console.log(candidateName.toUpperCase());
+// print variable in titlecase
+console.log(candidateName.replace('j', 'J'));
---
+++ day_2/main.ts
@@ -0,0 +1,13 @@
+// Sheihk Jibran Ahmed
+
+//create variable 
+let candidateName:string = 'jibran'
+
+// print variable in lowercase
+console.log(candidateName.toLowerCase())
+
+// print variable in uppercase
+console.log(candidateName.toUpperCase())
+
+// print variable in titlecase
+console.log(candidateName.replace('j','J'))
