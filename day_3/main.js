6c4397015a48d17a030f6570f94d17006e33499f
SJibran Ahmed <Sheikhjibran889970@gmail.com>
2024-03-07 07:03:17 +0500

    Famous Quotes

---
+++ day_3/main.js
@@ -0,0 +1,5 @@
+// Sheikh Jibran Ahmed
+//store name variable value 
+var ProphetName = 'Muhammad (P.B.U.H) ';
+//print variable in literal or template string
+console.log("".concat(ProphetName, "once said,\" Modesty is a part of imaan \"(Sahih Muslim:35)"));
---
+++ day_3/main.ts
@@ -0,0 +1,7 @@
+// Sheikh Jibran Ahmed
+
+//store name variable value 
+let ProphetName:string = 'Muhammad (P.B.U.H) '
+
+//print variable in literal or template string
+console.log(`${ProphetName}once said," Modesty is a part of imaan "(Sahih Muslim:35)`)
