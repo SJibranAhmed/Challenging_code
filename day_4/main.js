85d70e7cf0b53506184d2f0c09f7962dc4360d0b
SJibran Ahmed <Sheikhjibran889970@gmail.com>
2024-03-07 07:16:50 +0500

    famous quote 2

---
+++ day_4/main.js
@@ -0,0 +1,9 @@
+// Sheikh Jibran Ahmed
+//storing famous quote in variable
+var famousQuotes = 'Stuff doesn\'t need to change the world to be good.';
+//storing famous_person name 
+var famous_person = 'Elon Musk';
+//store both together variable  by template string
+var message = "".concat(famous_person, " said,\"").concat(famousQuotes, "\"");
+//print our message
+console.log(message);
---
+++ day_4/main.ts
@@ -0,0 +1,13 @@
+// Sheikh Jibran Ahmed
+
+//storing famous quote in variable
+let famousQuotes:string = 'Stuff doesn\'t need to change the world to be good.'
+
+//storing famous_person name 
+let famous_person:string = 'Elon Musk'
+
+//store both together variable  by template string
+let message:string = `${famous_person} said,"${famousQuotes}"`
+
+//print our message
+console.log(message)
